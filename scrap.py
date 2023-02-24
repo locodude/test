efe1406de3a96411191e1d95401ce38ed8baeaf8
Alex L. G <alexlg@atocatl.astroscu.unam.mx>
2023-02-24 15:50:09 -0600

    agregando paginas traducidas

--- scrap.py
+++ scrap.py
@@ -5,6 +5,7 @@ import re
 import translators.server as ts
 import sys
 import time
+import os
 from bs4 import BeautifulSoup, SoupStrainer
 
 def main():
@@ -13,8 +14,8 @@ def main():
   agent = {"User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
 
 
-  lab = ['p','h1','h2','h3','h4','a','span','title','strong','i','div']
-  #lab = ['p','h1','h2','h3','h4','a','span','title','strong','i']
+  #lab = ['p','h1','h2','h3','h4','a','span','title','strong','i','div']
+  lab = ['p','h1','h2','h3','h4','a','span','title','strong','i']
   
   content = requests.get(site_name, headers=agent).content
   
@@ -24,49 +25,44 @@ def main():
       links.append(link['href'])
   
   links = list(set([site_name+l for l in links if not re.search('http', l)]))
-  links = links[links.index(site_name+'/')]
-
+  ind = links.index(site_name+'/')
+  #links = [links[ind]] + links[ind+21:ind+50]
+  links = links[ind+21:ind+50]
+  
   #for link in links:
   #  print(link)
   
   for link in links:
-    # test_link = links[links.index(site_name+'/')]
-    # print(test_link)
+    print("\nProcessing page: {0} ({1} of {2})".format(link, links.index(link)+1, len(links)))
 
     text = requests.get(link, headers=agent).text
     par = BeautifulSoup(text, 'html.parser')
     
-    # html = '''
-    # <html>
-    # <head>
-    # <title>That is the display section.</title>
-    # </head>
-    # <body>
-    # <p>So got the stairs. <span>Hi dog</span> Crazy</p>
-    # <p></p>
-    # <p>Yes, said Arthur, yes I did. It was displayed at the bottom of the locked filing cabinet stuck in a dormant toilet with a sign on the door saying 'Be wary of the leopard'</p></body>
-    # </html>
-    # '''
-    #par = BeautifulSoup(html, 'html.parser')
-
     for i in par.findAll(lab):
-      if i.string is not None and len(str(chi.string).strip()) > 0:
+      if i.string is not None and len(str(i.string).strip()) > 0:
         sys.stdout.write('\x1b[2K')
-        print("\rTranslating: {0}".format(i.string), end="")
+        print("\r\tTranslating: {0}".format(i.string), end="")
         i.string.replace_with(ts.google(i.string, from_language='en', to_language='es'))
       else:
         for chi in i.children:
           if chi.string is not None and len(str(chi.string).strip()) > 0:
             sys.stdout.write('\x1b[2K')
-            print("\rTranslating: {0}".format(chi.string), end="")
-            time.sleep(0.5)
-            ns = ts.google(chi.string, from_language='en', to_language='es')
+            print("\r\tTranslating: {0}".format(chi.string), end="")
+            ns = ts.google(chi.string, from_language='en', to_language='hi')
             chi.string.replace_with(ns)
 
-    res = link.replace(site_name, "home_")
-    res = "_".join(res.split('/'))
+    if link == site_name+'/':
+      res = './index.html'
+    else:
+      res = link.replace(site_name, ".")
+      res = res+'/'+res.split('/')[-1]+'.html'
+   
     par = str(par)
-    f = open('./paginas/'+res, 'w+')
+
+    dir = os.path.dirname(res)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    f = open(res, 'w+')
     f.write(par)
     f.close()
 
